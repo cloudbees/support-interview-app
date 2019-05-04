08c739d25b2796b8137e998553fb8dc29628cef4
carlosrodlop <it.carlosrodlop>
2019-05-04 20:43:15 +0200

    adding config.xml for milestone 5 and 6

--- bash/createJobs.sh
+++ bash/createJobs.sh
@@ -4,7 +4,8 @@ ADMIN_TOKEN=$1
 
 if [[ -z "${ADMIN_TOKEN}" ]]; then
     echo "[ERROR]: Please add an admin API Token"
+    echo "[INFO]: Check https://stackoverflow.com/a/45466184 to understand how to create one"
 else
-    curl -s -XPOST 'http://localhost:8080/createItem?name=Milestone4' --data-binary @milestone-4.xml -H "Content-Type:text/xml" --user admin:$ADMIN_TOKEN
-    #curl -s -XPOST 'http://localhost:8080/createItem?name=Milestone5' --data-binary @milestone-5.xml -H "Content-Type:text/xml" --user admin:$ADMIN_TOKEN
+    curl -s -XPOST 'http://localhost:8080/createItem?name=Milestone5' --data-binary @milestone-5.xml -H "Content-Type:text/xml" --user admin:$ADMIN_TOKEN
+    curl -s -XPOST 'http://localhost:8080/createItem?name=Milestone6' --data-binary @milestone-6.xml -H "Content-Type:text/xml" --user admin:$ADMIN_TOKEN
 fi
