4da59f1c4be492b53c5b0d87ed9fbaa1ff62deba
carlosrodlop <it.carlosrodlop>
2019-05-15 18:26:23 +0200

    debuggin script

--- shell/createJobs.sh
+++ shell/createJobs.sh
@@ -7,5 +7,5 @@ if [[ -z "${ADMIN_TOKEN}" ]]; then
     echo "[INFO]: Check https://stackoverflow.com/a/45466184 to understand how to create one"
 else
     curl -s -XPOST 'http://localhost:8080/createItem?name=Milestone5' --data-binary @milestone-5.xml -H "Content-Type:text/xml" --user admin:$ADMIN_TOKEN
-    curl -s -XPOST 'http://localhost:8080/createItem?name=Milestone6' --data-binary @milestone-6.xml -H "Content-Type:text/xml" --user admin:$ADMIN_TOKEN
+    #curl -s -XPOST 'http://localhost:8080/createItem?name=Milestone6' --data-binary @milestone-6.xml -H "Content-Type:text/xml" --user admin:$ADMIN_TOKEN
 fi
