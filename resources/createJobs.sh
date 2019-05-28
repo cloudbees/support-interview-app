1ad7582f9271cbc7d5c1fc86c0215dadfe74730a
carlosrodlop <it.carlosrodlop>
2019-05-28 15:13:06 +0200

    typo job names

--- resources/createJobs.sh
+++ resources/createJobs.sh
@@ -7,7 +7,7 @@ JENKINS_URL=$2
 if [[ -z "${ADMIN_TOKEN}" ]]; then
     echo "[ERROR]: Please pass an admin API Token as parameter"
 else
-    curl -s -XPOST "$JENKINS_URL/createItem?name=Milestone 5" --data-binary @milestone-5.xml -H "Content-Type:text/xml" --user  Adm1n:$ADMIN_TOKEN
-    curl -s -XPOST "$JENKINS_URL/createItem?name=Milestone 6" --data-binary @milestone-6.xml -H "Content-Type:text/xml" --user  Adm1n:$ADMIN_TOKEN
+    curl -s -XPOST "$JENKINS_URL/createItem?name=Milestone_5" --data-binary @milestone-5.xml -H "Content-Type:text/xml" --user  Adm1n:$ADMIN_TOKEN
+    curl -s -XPOST "$JENKINS_URL/createItem?name=Milestone_6" --data-binary @milestone-6.xml -H "Content-Type:text/xml" --user  Adm1n:$ADMIN_TOKEN
 fi
 set +x
